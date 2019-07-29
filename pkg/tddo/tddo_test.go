a7ca9dcbae87891c9c4c153deb5bb76508086a3a
y.t <y.softvalley@gmail.com>
2019-07-29 23:12:07 +0900

    fix tests

--- pkg/tddo/tddo_test.go
+++ pkg/tddo/tddo_test.go
@@ -14,9 +14,7 @@ func TestExist(t *testing.T) {
 		{"hoge.go", false},
 	}
 	for _, test := range existsTests {
-		td := Tddo{
-			Name: test.in,
-		}
+		td := &Tddo{Name: test.in}
 		t.Run(test.in, func(t *testing.T) {
 			if expected, actual := test.out, td.Exists(); expected != actual {
 				t.Errorf("td.Exists wont %t but got %t", expected, actual)
@@ -38,9 +36,7 @@ func TestConfirm(t *testing.T) {
 	}
 	for _, test := range confirmTests {
 		t.Run(test.in, func(t *testing.T) {
-			td := &Tddo{
-				Reader: bytes.NewBufferString(test.in),
-			}
+			td := &Tddo{Reader: bytes.NewBufferString(test.in)}
 			if expected, actual := test.out, td.Confirm(); expected != actual {
 				t.Errorf("td.confirm() wont %t but got %t", expected, actual)
 			}
