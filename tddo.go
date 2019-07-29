6c29200e010a09ed198f79ded0a2a73b355ca197
y.t <y.softvalley@gmail.com>
2019-07-29 14:58:42 +0900

    add confirm

--- tddo.go
+++ tddo.go
@@ -1,20 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-// FILENAME is target todo file.
-const FILENAME = "tddo.md"
+const (
+	// FILENAME is target todo file.
+	FILENAME = "tddo.md"
+
+	// TEMPLATE is todo file template.
+	TEMPLATE = `# TODO for test-driven development
+
+- [x] Create TODO
+- [ ]
+- [ ]
+- [ ]
+- [ ]
+`
+)
 
 func run() {
-	if ok := exists(FILENAME); ok {
+	if ok := exists(FILENAME); !ok {
 		fmt.Printf("%s is already exists", FILENAME)
+		return
 	}
+	confirm(os.Stdin)
 }
 
 func exists(name string) bool {
 	_, err := os.Stat(name)
 	return !os.IsNotExist(err)
+}
+
+func confirm(r io.Reader) bool {
+	sc := bufio.NewScanner(r)
+	sc.Scan()
+	if sc.Text() != "Y" {
+		return false
+	}
+	return true
+}
+
+func create(name, text string) error {
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0755)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write([]byte(text))
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- tddo_test.go
+++ tddo_test.go
@@ -1,10 +1,44 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestExist(t *testing.T) {
-	fn := "README.md"
-	if expected, actual := exists(fn), true; expected != actual {
-		t.Errorf("exists(%s) wont %t but got %t", fn, expected, actual)
+	existsTests := []struct {
+		in  string
+		out bool
+	}{
+		{"README.md", true},
+		{"hoge.md", false},
+	}
+	for _, test := range existsTests {
+		t.Run(test.in, func(t *testing.T) {
+			if expected, actual := test.out, exists(test.in); expected != actual {
+				t.Errorf("esists(%s) wont %t but got %t", test.in, expected, actual)
+			}
+
+		})
+	}
+
+}
+
+func TestConfirm(t *testing.T) {
+	confirmTests := []struct {
+		in  string
+		out bool
+	}{
+		{"Y", true},
+		{"n", false},
+		{"other", false},
+	}
+	for _, test := range confirmTests {
+		t.Run(test.in, func(t *testing.T) {
+			if expected, actual := test.out, confirm(bytes.NewBufferString(test.in)); expected != actual {
+				t.Errorf("confirm(%s) wont %t but got %t", test.in, expected, actual)
+			}
+
+		})
 	}
 }
